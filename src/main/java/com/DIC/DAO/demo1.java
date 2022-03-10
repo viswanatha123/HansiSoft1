10da71f2bf894bfcc39e8b93e920d199df90fde4
viswanatha123 <101308882+viswanatha123@users.noreply.github.com>
2022-03-10 14:41:44 +0530

    Update demo1.java

--- src/main/java/com/DIC/DAO/demo1.java
+++ src/main/java/com/DIC/DAO/demo1.java
@@ -1,12 +1,8 @@
 package com.DIC.DAO;
 
 public class demo1 {
-	
-<<<<<<< HEAD
+
 	public void test()
-=======
-	public void dis()
->>>>>>> 31f40522336aa8846144e356be8cef33b7ea9e80
 	{
 		System.out.println("welcome to India");
 	}
